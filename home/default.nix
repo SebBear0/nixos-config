89d8a2530f3d46248c890365ad968c68a76a1931
Seb Bearsley <sebastian.bearsley@gmail.com>
2026-01-07 19:41:58 +1100

    Added relative-motions yazi plugin

--- home/default.nix
+++ home/default.nix
@@ -36,6 +36,7 @@
     # '')
 
     pkgs.ghostty
+    pkgs.yaziPlugins.relative-motions
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
--- home/yazi/default.nix
+++ home/yazi/default.nix
@@ -8,5 +8,8 @@
 
   home.file.".config/yazi/catppuccin-mocha.tmTheme".source = ./catppuccin-mocha.tmTheme;
   home.file.".config/yazi/catppuccin-mocha.tmTheme".force = true;
+
+  home.file.".config/yazi/init.lua".source = ./init.lua;
+  home.file.".config/yazi/init.lua".force = true;
 }
 
