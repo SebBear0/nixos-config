06595c8775e3e60121207996fe813a606ec17584
Seb Bearsley <sebastian.bearsley@gmail.com>
2026-01-20 18:57:21 +1100

    added relative line numbers to yazi

--- home/yazi/default.nix
+++ home/yazi/default.nix
@@ -2,11 +2,20 @@
   pkgs,
   config,
   ...
-}: {
+}:
+{
   home.file.".config/yazi/theme.toml".source = ./theme.toml;
   home.file.".config/yazi/theme.toml".force = true;
 
   home.file.".config/yazi/flavors".source = ./flavors;
   home.file.".config/yazi/flavors".force = true;
-}
 
+  home.file.".config/yazi/plugins".source = ./plugins;
+  home.file.".config/yazi/plugins".force = true;
+
+  home.file.".config/yazi/init.lua".source = ./init.lua;
+  home.file.".config/yazi/init.lua".force = true;
+
+  home.file.".config/yazi/keymap.toml".source = ./keymap.toml;
+  home.file.".config/yazi/keymap.toml".force = true;
+}
